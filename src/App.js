775c6bd0420f4f6daa2e6c19f3fa620d73002448
Bowie <lfranckx@thinkful.com>
2022-03-02 16:25:42 -0600

    finish rating update functionality

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import data from './data';
 import { GrStar } from 'react-icons/gr';
 
 function App() {
+  const [ratings, setRatings] = useState({});
+
+  useEffect(() => {
+    setRatings(data.map((product, i) => {
+      return { rating: product.rating };
+    }))
+  }, [])
+
+  const updateRatings = (i, rating) => {
+    console.log(`index: ${i}`, `rating: ${rating}`);
+    let newRatings = [...ratings];
+    newRatings[i] = {rating: rating};
+    setRatings(newRatings);
+  }
 
   const productCards = data.map((product, i) => {
-    
-    return (
-      <li className='card' key={i}>
-        <div className='image-wrap'>
-          <img src='images/tshirt.png' alt='black t-shirt' />
-        </div>
+    // save object index of product to update rating
+    const objIndex = i;
 
-        <div className='content-wrap'>
-          <h3>{product.name}</h3>
-          <p>{product.price}</p>
-          <div className='star-wrap'>
-            {[...Array(5)].map((star, i) => {
-              return (
-                <label key={i} >
-                  <input 
-                    type={"radio"} 
-                    name='rating' 
-                    value={i} 
-                  />
-                  <GrStar 
-                    className='star' 
-                    color={i < product.rating ? "#000" : "#cbcbcb"} 
-                  />
-                </label>
-              )
-            })}
+    if (ratings.length > 0) {
+      return (
+        <li className='card' key={i}>
+          <div className='image-wrap'>
+            <img src='images/tshirt.png' alt='black t-shirt' />
           </div>
-        </div>
-
-        <div className='btn-wrap'>
-          <button>Add to cart</button>
-        </div>
-
-        <div className='overlay'></div>
-      </li>
-    )
+  
+          <div className='content-wrap'>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <div className='star-wrap'>
+              {[...Array(5)].map((star, i) => {
+                const starRating = i + 1;
+                return (
+                  <label key={i} >
+                    <input 
+                      type="radio" 
+                      name='rating' 
+                      value={starRating} 
+                      onClick={() => updateRatings(objIndex, starRating)}
+                    />
+                    <GrStar 
+                      className='star' 
+                      color={starRating <= ratings[objIndex].rating ? "#000" : "#cbcbcb"} 
+                    />
+                  </label>
+                )
+              })}
+            </div>
+          </div>
+  
+          <div className='btn-wrap'>
+            <button>Add to cart</button>
+          </div>
+  
+          <div className='overlay'></div>
+        </li>
+      )
+    }
   }); 
 
   return (
