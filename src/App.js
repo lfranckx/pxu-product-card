8dbe70e883049cebf2f69983d484e6c26f90425d
Bowie <lfranckx@thinkful.com>
2022-03-02 10:25:32 -0600

    add styling / install react icons / start star rating functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import './App.scss';
+import data from './data';
 
 function App() {
+  const productCards = data.map((product, i) => {
+    return (
+      <li className='card' key={i}>
+        <div className='image-wrap'>
+          <img src='images/tshirt.png' alt='black t-shirt' />
+        </div>
+
+        <div className='content-wrap'>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
+          <p>{product.rating}</p>
+        </div>
+        
+        <div className='btn-wrap'>
+          <button>Add to cart</button>
+        </div>
+      </li>
+    )
+  });
+
   return (
     <>
+      <ul className='products'>
+        {productCards}
+      </ul>
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        name: 'Ultrafine Merino T-Shirt',
+        price: '$80.00',
+        rating: 4
+    },
+    {
+        name: 'Ultrafine Merino T-Shirt',
+        price: '$80.00',
+        rating: 5
+    },
+    {
+        name: 'Ultrafine Merino T-Shirt',
+        price: '$80.00',
+        rating: 3.5
+    },
+]
+
+export default data;
